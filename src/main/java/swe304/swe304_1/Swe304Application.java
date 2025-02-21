598be3e07d29941d6c3ffc401d7d1c152aed685b
Vitalii Korniichuk <ipz215_kvv@student.ztu.edu.ua>
2025-02-21 18:26:59 +0300

    build: use env for properties

--- src/main/java/swe304/swe304_1/Swe304Application.java
+++ src/main/java/swe304/swe304_1/Swe304Application.java
@@ -1,5 +1,6 @@
 package swe304.swe304_1;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Swe304Application {
 
 	public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+
+        System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
+        System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
+        System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
+        System.setProperty("DB_USER", dotenv.get("DB_USER"));
+        System.setProperty("DB_PASS", dotenv.get("DB_PASS"));
+
 		SpringApplication.run(Swe304Application.class, args);
 	}
 
