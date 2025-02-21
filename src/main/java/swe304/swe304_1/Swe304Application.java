52975347ea81f27b6d05d199481fd82861962c6b
Vitalii Korniichuk <ipz215_kvv@student.ztu.edu.ua>
2025-02-21 17:38:56 +0300

    feat: set up project

---
+++ src/main/java/swe304/swe304_1/Swe304Application.java
@@ -0,0 +1,13 @@
+package swe304.swe304_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swe304Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Swe304Application.class, args);
+	}
+
+}
