50dead3ef3df215d8480d97ed7a87aa899a646f0
Vitalii Korniichuk <ipz215_kvv@student.ztu.edu.ua>
2025-03-01 15:14:16 +0300

    feat: add stats to main page

--- src/main/java/swe304/swe304_1/controller/SiteController.java
+++ src/main/java/swe304/swe304_1/controller/SiteController.java
@@ -4,21 +4,44 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import swe304.swe304_1.repository.BuildingRepository;
+import swe304.swe304_1.repository.PersonRepository;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 
 @Controller
 @RequestMapping("/")
 public class SiteController {
+    private final BuildingRepository buildingRepository;
+    private final PersonRepository personRepository;
 
-//    @Autowired
-//    public SiteController() {
-//
-//    }
+    public SiteController(BuildingRepository buildingRepository, PersonRepository personRepository) {
+        this.buildingRepository = buildingRepository;
+        this.personRepository = personRepository;
+    }
 
     @GetMapping("")
     public String index(Model model) {
+        long buildingCount = buildingRepository.count();
+        long personCount = personRepository.count();
+
+        Set<String> uniqueCountries = buildingRepository.findAll().stream()
+                .map(building -> building.getCountry().toLowerCase()) // Normalize to lowercase
+                .collect(Collectors.toSet());
+
+        Set<String> uniqueOccupations = personRepository.findAll().stream()
+                .map(person -> person.getOccupation().toLowerCase()) // Normalize to lowercase
+                .collect(Collectors.toSet());
+
         model.addAttribute("title", "SWE 304");
         model.addAttribute("view", "site/index");
+        model.addAttribute("buildingCount", buildingCount);
+        model.addAttribute("personCount", personCount);
+        model.addAttribute("uniqueCountryCount", uniqueCountries.size());
+        model.addAttribute("uniqueOccupationCount", uniqueOccupations.size());
+
         return "layout";
     }
 
