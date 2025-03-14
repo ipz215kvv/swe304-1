b2d0907dc93e5b2257a44b48bdf4bcaf14dbcb4f
Vitalii Korniichuk <ipz215_kvv@student.ztu.edu.ua>
2025-03-14 10:44:15 +0300

    feat!: configure docker image
    BREAKING CHANGE: remove support of working without docker

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM openjdk:17-jdk-slim
+
+WORKDIR /usr/share/java
+
+COPY ./build/libs/swe304_1-1.0.0.jar app.jar
+
+ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/java/swe304/swe304_1/Swe304Application.java
+++ src/main/java/swe304/swe304_1/Swe304Application.java
@@ -1,6 +1,5 @@
 package swe304.swe304_1;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,14 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Swe304Application {
 
 	public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load();
-
-        System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
-        System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
-        System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
-        System.setProperty("DB_USER", dotenv.get("DB_USER"));
-        System.setProperty("DB_PASS", dotenv.get("DB_PASS"));
-
 		SpringApplication.run(Swe304Application.class, args);
 	}
 
