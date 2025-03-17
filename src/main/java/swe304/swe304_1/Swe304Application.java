b7817929d98db2236a2a7b0b99dc4b9ba4e3caa8
Vitalii Korniichuk <ipz215_kvv@student.ztu.edu.ua>
2025-03-17 10:10:19 +0300

    refactor: add support to non-docker db

--- src/main/java/swe304/swe304_1/Swe304Application.java
+++ src/main/java/swe304/swe304_1/Swe304Application.java
@@ -3,11 +3,30 @@ package swe304.swe304_1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
+
 @SpringBootApplication
 public class Swe304Application {
 
 	public static void main(String[] args) {
+        try {
+            Dotenv dotenv = Dotenv.load();
+			setSystemProperty("MYSQL_HOST", dotenv.get("MYSQL_HOST"));
+			setSystemProperty("MYSQL_PORT", dotenv.get("MYSQL_PORT"));
+			setSystemProperty("MYSQL_DATABASE", dotenv.get("MYSQL_DATABASE"));
+        } catch (Exception e) {
+			
+		}
+		
+
 		SpringApplication.run(Swe304Application.class, args);
 	}
+
+	private static void setSystemProperty(String key, String value) {
+        if (System.getProperty(key) == null && value != null) {
+            System.setProperty(key, value);
+        }
+    }
 
 }
